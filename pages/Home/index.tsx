4fa8cdb4ea229798d9d641252017ac3963d94196
Davi Santos <davidossantosfreitas19@gmail.com>
2022-04-27 21:23:27 -0300

    add home screen

---
+++ pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import { Container } from "./style";
+
+export const HomePage: NextPage = () => {
+    return (
+        <Container>
+            <h1>Ramírez</h1>
+        </Container>
+    )
+}
---
+++ pages/Home/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { pallete } from "../../styles/colors";
+
+export const Container = styled("div")`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: ${pallete.blackOne};
+    color: ${pallete.whietOne};
+`

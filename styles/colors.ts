566c6d7c5dbc9b91b95abab64f4769596c36b50d
Davi Santos <davidossantosfreitas19@gmail.com>
2022-04-27 21:22:59 -0300

    add pallete

---
+++ styles/colors.ts
@@ -0,0 +1,13 @@
+export const pallete = {
+    blackOne: "#212121",
+    blackTwo: "#434343",
+    blackThree: "#272727",
+    blackFour: "#545454",
+    blackFive: "#121214",
+    blackSix: "#313131",
+    grayOne: "#c4c4c4",
+    grayTwo: "#979797",
+    grayThree: "#bbbbbb",
+    whietOne: "#ffffff",
+    turquoiseOne: "#40e0d0"
+}

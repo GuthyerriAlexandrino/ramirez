363c4da1b2f5e1f472a5899be36548a9ef2798b1
Davi Santos <davidossantosfreitas19@gmail.com>
2022-05-07 00:52:10 -0300

    change font

--- styles/global.ts
+++ styles/global.ts
@@ -11,6 +11,6 @@ export default createGlobalStyle`
     body {
         background: ${pallete.whietOne};
         font-size: 16px;
-        font-family: sans-serif;
+        font-family: 'Montserrat', sans-serif;
     }
 `

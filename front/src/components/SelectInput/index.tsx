6419ddd5bd0a19b701c5f6e5b51811873e0fcce0
Davi Santos <davidossantosfreitas19@gmail.com>
2022-05-15 15:11:04 -0300

    develop select input

--- front/src/components/SelectInput/index.tsx
+++ front/src/components/SelectInput/index.tsx
@@ -1,18 +1,42 @@
-import { Container, Icon } from "./style";
+import { Container, Icon, PopupContainer, PopupItem, PopupList } from "./style";
 import Select from "../../assets/select.svg"
 import Image from "next/image";
+import { useState } from "react";
 
 interface SelectInputProps {
-    name: string
+    name: string;
+    selectName: string;
 }
 
-export function SelectInput({name}: SelectInputProps) {
+const data = [
+    {id: 1, value: "Fotojornalismo"},
+    {id: 2, value: "Gastronomia"},
+    {id: 3, value: "Astrofotografia"},
+    {id: 4, value: "Publicidade"},
+    {id: 5, value: "Subaquática"},
+    {id: 6, value: "Aérea"},
+]
+
+export function SelectInput({name, selectName}: SelectInputProps) {
+
+    const [currentName, setCurrentName] = useState(selectName);
+    const [isActive, setIsActive] = useState(false);
+
     return (
-        <Container>
-            <small>{name}</small>
+        <Container onClick={() => setIsActive(!isActive)}>
+            <small>{currentName}</small>
             <Icon>
                 <Image src={Select} width={15} height={15}/>
             </Icon>
+            <PopupContainer isActive={isActive}>
+                <PopupList isActive={isActive}>
+                    {data.map((item, id) => (
+                        <PopupItem tabIndex={id} onClick={() => setCurrentName(item.value)}>
+                            {item.value}
+                        </PopupItem>
+                    ))}
+                </PopupList>
+            </PopupContainer>
         </Container>
     )
 }

85e63b9df07c427089ea039d1fee220cd5883ffe
Davi Santos <davidossantosfreitas19@gmail.com>
2022-05-23 15:41:45 -0300

    add notification context

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from '../context/AuthContext';
+import { NotifyProvider } from '../context/NotifyContext';
 import GlobalStyle from "../styles/global";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle/>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <NotifyProvider>
+        <AuthProvider>
+            <Component {...pageProps} />
+            <ToastContainer />
+        </AuthProvider>
+      </NotifyProvider>
     </>
   )
 }

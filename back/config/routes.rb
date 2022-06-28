da79fbbb4d16f62e73e7c50bc4daa07bb9bf52d2
GuthyerriAlexandrino <galexandrino4@gmail.com>
2022-06-28 13:03:42 -0300

    Create post route and com_like route updated/added

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   resources :views
   # resources :answers
-  # resources :comments
+  resources :comments
   resources :posts
   resources :specializations
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  # root "users#index"
   post '/login', to: 'registration#login'
   post '/register', to: 'registration#register'
   get '/setimg', to: 'users#set_img'
-  post '/matarocris', to: 'posts#create'
+  post '/comments/:id', to: 'comments#like'
 end

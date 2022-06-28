b5ae11e41e19dc62e2139a60598cec532b018427
GuthyerriAlexandrino <galexandrino4@gmail.com>
2022-06-28 18:17:36 -0300

    Updated to include services_price

--- back/app/services/user_service.rb
+++ back/app/services/user_service.rb
@@ -1,11 +1,11 @@
 module UserService
   def UserService.all_permited
     [:name, :email, :photographer, :password, :password_confirmation,
-      :city, :state, :services_price,
+      :city, :state, :services_price => [],
       :bio, :profile_img, :specialization => []]
   end
 
   def UserService.search_view
-    [:name, :email, :specialization, :services_price, :city, :state, :views]
+    [:name, :email, :profile_img, :specialization, :services_price, :city, :state, :views]
   end
 end

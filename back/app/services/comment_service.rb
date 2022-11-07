74d30d8eb9e02f57a2b0f53678ba8f4e19058738
GuthyerriAlexandrino <galexandrino4@gmail.com>
2022-11-06 21:48:12 -0300

    refact: adds get_comment method to CommentService

--- back/app/services/comment_service.rb
+++ back/app/services/comment_service.rb
@@ -1,3 +1,12 @@
-module CommentService
+# frozen_string_literal: true
 
+module CommentService
+  def self.get_comment(comment_id, post)
+    comment = post.comments&.find(comment_id)
+    raise InvalidCommentException.new, "This comment don't exists in the specified post" if comment.nil?
+
+    comment
+  end
+
+  class InvalidCommentException < StandardError; end
 end

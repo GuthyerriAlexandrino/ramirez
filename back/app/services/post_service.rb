b185d18fdb6b7f6737dcc47bccf6d340a833bbf8
GuthyerriAlexandrino <galexandrino4@gmail.com>
2022-11-06 21:48:58 -0300

    refact: adds get_post to PostService

--- back/app/services/post_service.rb
+++ back/app/services/post_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module PostService
   def self.user_posts(user_id)
     user = User.find(_id: user_id)
@@ -5,13 +7,20 @@ module PostService
   end
 
   def self.post_params(title, price, uri)
-    post = { title: title, image: uri }
+    post = { title:, image: uri }
     post[:price] = price.to_f unless price.nil?
     post
   end
 
   def self.parse_filename(user_name, content_type)
     ext = Rack::Mime::MIME_TYPES.invert[content_type]
-    filename = "#{user_name}/#{SecureRandom.uuid}#{ext}"
+    "#{user_name}/#{SecureRandom.uuid}#{ext}"
+  end
+
+  def self.get_post(author_id, post_id)
+    author = User.find(author_id)
+    raise(UserService::InvalidUserException.new, 'Invalid post author') if author.nil?
+
+    author.posts&.find(post_id)
   end
 end

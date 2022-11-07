ea7eda0fc9b4d03cc7baa890ecf8792f461bf51a
GuthyerriAlexandrino <galexandrino4@gmail.com>
2022-11-06 21:49:26 -0300

    refact: adds custom user exception

--- back/app/services/user_service.rb
+++ back/app/services/user_service.rb
@@ -8,4 +8,6 @@ module UserService
   def UserService.search_view
     [:name, :email, :profile_img, :specialization, :services_price, :city, :state, :views]
   end
+
+  class InvalidUserException < StandardError; end
 end

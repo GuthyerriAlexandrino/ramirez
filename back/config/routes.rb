0b5b2102d356b963a07991e886a08e1f9da2a1fb
GuthyerriAlexandrino <galexandrino4@gmail.com>
2022-11-17 17:17:19 -0300

    style: removes whitespaces

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  
+
   # Register routes
   post '/login', to: 'registration#login'
   post '/register', to: 'registration#register'
-  
+
   # Post routes
   get '/posts/:id', to: 'posts#index'
   get '/post/:u_id/:p_id', to: 'posts#show'
@@ -27,5 +27,5 @@ Rails.application.routes.draw do
   # User extra routes
   put '/user/profile_image', to: 'users#profile_image'
   get '/user/:id', to: 'users#user_data'
-
+  
 end

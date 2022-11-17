537f53d5aed3de47d905ad3b1589ca3477ec5e73
GuthyerriAlexandrino <galexandrino4@gmail.com>
2022-11-17 17:56:45 -0300

    feat: adds length validation to answer

--- back/app/models/answer.rb
+++ back/app/models/answer.rb
@@ -4,6 +4,10 @@ class Answer
   field :user_id, type: BSON::ObjectId
   field :answer_ref_id, type: BSON::ObjectId, default: nil
   field :content, type: String
+
+  # Validations
+  validates_length_of :content, minimum: 1, maximum: 500
+
   embedded_in :comment
   embeds_many :likes
 end

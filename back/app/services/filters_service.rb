d5eac7cdd6531d956a9d592b4f0a915d5386d0b7
GuthyerriAlexandrino <galexandrino4@gmail.com>
2022-11-29 15:50:53 -0300

    fix: fixes long time bugs in the service

--- back/app/services/filters_service.rb
+++ back/app/services/filters_service.rb
@@ -1,45 +1,40 @@
 module FiltersService
   def self.matching_params(params)
     new_hash = { photographer: true }
-    params.each_pair { |k, v| new_hash[k.to_sym] = v if self.check_param(k, v) }
-    new_hash[:name] = /.*#{params[:name]}.*/ unless params[:name].nil? || params[:name] == ""
+    params.each_pair { |k, v| new_hash[k.to_sym] = v if check_param(k, v) }
+    new_hash[:name] = /.*#{params[:name]}.*/ unless !params.key?(:name) || params[:name] == ''
     new_hash[:specialization.exists] = true
     new_hash
   end
   
   def self.location_params(location)
     locate = []
-    locate = [{ city: location }, { state: location }] unless location.nil? || location == ""
+    locate = [{ city: location }, { state: location }] unless location.nil? || location == ''
     locate
   end
 
   def self.order_params(order_by)
     order = {}
-    order = {order_by.to_sym => :desc} if order_by != "" && %w[likes views price].include?(order_by)
+    order = { order_by.to_sym => :desc } if order_by != '' && %w[likes views price].include?(order_by)
     order
   end
 
   def self.price_params(params)
-    price = {}
     min_price = (params[:min_price].nil? ? nil : params[:min_price].to_f)
     max_price = (params[:max_price].nil? ? nil : params[:max_price].to_f)
-    return {} if min_price.nil? && max_price.nil?
 
-    if min_price.nil?
-      price = {:services_price.elem_match => {:$lte => max_price} }
-    elsif max_price.nil?
-      price = {:services_price.elem_match => {:$gte => min_price} }
-    else 
-      price = {:services_price.elem_match => {:$gte => min_price, :$lte => max_price} }
-    end   
-    price
+
+    return {} if min_price.nil? && max_price.nil?
+    return { :services_price.elem_match => { :$lte => max_price } } if min_price.nil?
+    return { :services_price.elem_match => { :$gte => min_price } } if max_price.nil?
+
+    { :services_price.elem_match => { :$gte => min_price, :$lte => max_price } }
   end
 
   def self.check_pagination(page)
-    page.nil? || (Integer(params[:page]) rescue false)
+    page.nil? || page.is_a?(Integer) || (page.is_a?(String) && !Integer(page).nil?)
   end
 
-  private
   def self.check_param(key, value)
     key = key.to_sym
     condition1 = %i[name specialization].include?(key)
@@ -47,4 +42,6 @@ module FiltersService
 
     condition1 && condition2
   end
+
+  private_class_method :check_param
 end

c47acd8b5ed8a3ad0334d75ecc078a671cbf58f6
Guthyerri Alexandrino <31360386+GuthyerriAlexandrino@users.noreply.github.com>
2022-12-06 10:42:44 -0300

    Update production.rb

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Firebase auth
-  #export GOOGLE_APPLICATION_CREDENTIALS="../ramirez_firebase.key"
+  export GOOGLE_APPLICATION_CREDENTIALS= ${FIREK}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
